743fe85d8d501d23e1d70ed7fe48aac2e9d97766
Alistair Green <alistair.green@alumni-oxford.com>
2015-04-21 21:48:33 +0100

    Removed extraneous paragraph break from map attribution.
    Removed extraneous code to set default view on the map.

--- igcviewer.js
+++ igcviewer.js
@@ -17,9 +17,9 @@ $(document).ready(function() {
                      gpsBarogramData.push([timestamp, model.gpsAltitude[j]]);					 
 				 }
 				 
-				 var map = L.map('map').setView([51.505, -0.09], 13);
+				 var map = L.map('map');
 				 L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
-                              attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, <p>Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"></p>',
+                              attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
                               maxZoom: 18
                             }).addTo(map);
 				 
