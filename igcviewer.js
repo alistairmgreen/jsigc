30442a0e7f24af068ea683ce9d3548a4fc078cff
Richard Brisbourne <richard@rbris.me.uk>
2015-12-01 16:09:32 +0000

    Fixed bug where "zoom to tp" buttons overflowed fixed height div into barogram in narrow displays Cleaned up html.  Removed deprecated "<br clear=all>" and replaced with css. Removed deprecated "autocomplete=off". Autocompleted unit preference is now read in "onload", and unit conversion constant set there. Moved "load igc file" to top centre

--- igcviewer.js
+++ igcviewer.js
@@ -411,7 +411,15 @@ function gettimezone(igcFile,mapControl)  {
 
 
     $(document).ready(function () {
-        var mapControl = createMapControl('map');        
+        var mapControl = createMapControl('map');  
+        var altitudeUnit = $('#altitudeUnits').val();
+            if (altitudeUnit === 'feet') {
+                altitudeConversionFactor = 3.2808399;
+            }
+            else {
+                altitudeConversionFactor = 1.0;
+            }
+            
         $('#fileControl').change(function () {
             if (this.files.length > 0) {
                 var reader = new FileReader();
@@ -512,5 +520,5 @@ function gettimezone(igcFile,mapControl)  {
              }
          });
     });
-            
+ 
 }(jQuery));
